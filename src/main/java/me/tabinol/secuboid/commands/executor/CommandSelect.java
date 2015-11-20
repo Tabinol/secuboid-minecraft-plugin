becae752942f4c441f1b2848c46b8fb32332b904
Tabinol <8842894+Tabinol@users.noreply.github.com>
2015-11-20 12:13:32 -0500

    Change Item ID to Item name and add command aliases

--- src/main/java/me/tabinol/secuboid/commands/executor/CommandSelect.java
+++ src/main/java/me/tabinol/secuboid/commands/executor/CommandSelect.java
@@ -42,7 +42,7 @@ import org.bukkit.entity.Player;
 /**
  * The Class CommandSelect.
  */
-@InfoCommand(name="select")
+@InfoCommand(name="select", aliases={"sel"})
 public class CommandSelect extends CommandExec {
 
     /** The player. */
@@ -107,7 +107,7 @@ public class CommandSelect extends CommandExec {
             if (!argList.isLast()) {
 
                 curArg = argList.getNext();
-                if (curArg.equalsIgnoreCase("worldedit")) {
+                if (curArg.equalsIgnoreCase("worldedit") || curArg.equalsIgnoreCase("wd")) {
                     if (Secuboid.getThisPlugin().getDependPlugin().getWorldEdit() == null) {
                         throw new SecuboidCommandException("CommandSelect", player, "COMMAND.SELECT.WORLDEDIT.NOTLOAD");
                     }

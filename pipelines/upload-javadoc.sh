3a0d113b8cbaf5b55bd69b4cf137e9da62e3f90a
Tabinol <8842894+Tabinol@users.noreply.github.com>
2019-10-12 21:53:44 -0400

    Change bash for sh

--- pipelines/upload-javadoc.sh
+++ pipelines/upload-javadoc.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
+# Bash not found in the cschlosser/alpine-lftps image
 
 lftp ftps://${FTP_TABINOL_ME_USER}:${FTP_TABINOL_ME_PASSWD}@ftp.tabinol.me -e "cd /javadoc; rm -r ${BITBUCKET_REPO_SLUG}; mkdir -p ${BITBUCKET_REPO_SLUG}; cd ${BITBUCKET_REPO_SLUG}; mput target/site/apidocs/*; bye"

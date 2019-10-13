5ed3651eae2dcb47ca2872831063e96332dcee08
BuildTools <Tabinol>
2019-10-12 22:25:58 -0400

    Change lftp command

--- pipelines/upload-javadoc.sh
+++ pipelines/upload-javadoc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Bash not found in the cschlosser/alpine-lftps image
 
-lftp ftps://${FTP_TABINOL_ME_USER}:${FTP_TABINOL_ME_PASSWD}@ftp.tabinol.me -e "cd /javadoc; rm -r ${BITBUCKET_REPO_SLUG}; mkdir -p ${BITBUCKET_REPO_SLUG}; cd ${BITBUCKET_REPO_SLUG}; mput target/site/apidocs/*; bye"
+lftp -u ${FTP_TABINOL_ME_USER},${FTP_TABINOL_ME_PASSWD} ftp.tabinol.me -e "set ftp:ssl-allow no; mkdir -p ${BITBUCKET_REPO_SLUG}; mirror  -e -R target/site/apidocs/ javadoc/${BITBUCKET_REPO_SLUG}/; bye"

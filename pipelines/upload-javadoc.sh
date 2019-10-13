6403d89869119c2266f0c799594f7d1e90735961
Tabinol <8842894+Tabinol@users.noreply.github.com>
2019-10-12 22:36:13 -0400

    Pipeline remove mkdir

--- pipelines/upload-javadoc.sh
+++ pipelines/upload-javadoc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Bash not found in the cschlosser/alpine-lftps image
 
-lftp -u ${FTP_TABINOL_ME_USER},${FTP_TABINOL_ME_PASSWD} ftp.tabinol.me -e "set ftp:ssl-allow no; mkdir -p ${BITBUCKET_REPO_SLUG}; mirror  -e -R target/site/apidocs/ javadoc/${BITBUCKET_REPO_SLUG}/; bye"
+lftp -u ${FTP_TABINOL_ME_USER},${FTP_TABINOL_ME_PASSWD} ftp.tabinol.me -e "set ftp:ssl-allow no; mirror  -e -R target/site/apidocs/ javadoc/${BITBUCKET_REPO_SLUG}/; bye"

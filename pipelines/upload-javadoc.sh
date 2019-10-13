b2e82f7ab7befc2ca55873b38baa17799c2ab1c6
Tabinol <8842894+Tabinol@users.noreply.github.com>
2019-10-12 21:21:58 -0400

    Change pipeline for new repo deploy

---
+++ pipelines/upload-javadoc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+lftp ftps://${FTP_TABINOL_ME_USER}:${FTP_TABINOL_ME_PASSWD}@ftp.tabinol.me -e "cd /javadoc; rm -r ${BITBUCKET_REPO_SLUG}; mkdir -p ${BITBUCKET_REPO_SLUG}; cd ${BITBUCKET_REPO_SLUG}; mput target/site/apidocs/*; bye"

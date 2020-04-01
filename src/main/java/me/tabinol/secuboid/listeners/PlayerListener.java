e7b636b4e745008e084209e945efe3d07c92d349
Tabinol <mblanchet@fastmail.com>
2020-04-01 19:44:57 -0400

    #54 Impossible to put a sign inside a land when the floor or wall is outside

--- src/main/java/me/tabinol/secuboid/listeners/PlayerListener.java
+++ src/main/java/me/tabinol/secuboid/listeners/PlayerListener.java
@@ -25,7 +25,6 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
-import org.bukkit.block.data.Rotatable;
 import org.bukkit.block.data.type.Furnace;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
@@ -431,7 +430,7 @@ public final class PlayerListener extends CommonListener implements Listener {
 
                 // For head place fix (do not spawn a wither)
             } else if (player.getEquipment().getItemInMainHand() != null && action == Action.RIGHT_CLICK_BLOCK
-                    && Rotatable.class.isAssignableFrom(player.getEquipment().getItemInMainHand().getType().data)
+                    && player.getEquipment().getItemInMainHand().getType() == Material.WITHER_SKELETON_SKULL
                     && (landPermissionsFlags.isBanned(event.getPlayer()) || !checkPermission(landPermissionsFlags,
                             event.getPlayer(), PermissionList.BUILD_PLACE.getPermissionType()))) {
                 messagePermission(player);

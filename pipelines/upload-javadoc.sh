e783fdc57aec67d479bf8847dfe7bb998d429b6d
Tabinol <8842894+Tabinol@users.noreply.github.com>
2020-05-28 21:54:48 -0400

    Circleci config 1

--- pipelines/upload-javadoc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Bash not found in the cschlosser/alpine-lftps image
-
-lftp -u ${FTP_TABINOL_ME_USER},${FTP_TABINOL_ME_PASSWD} ftp.tabinol.me -e "set ftp:ssl-allow no; mirror  -e -R target/site/apidocs/ javadoc/${BITBUCKET_REPO_SLUG}/; bye"
